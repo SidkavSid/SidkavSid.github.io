fa45881912b8218e6d83a860352935570647f87c
featherinst <59516863+featherinst@users.noreply.github.com>
2020-02-29 20:42:48 +0200

    1

---
+++ script.js
@@ -0,0 +1,21 @@
+$('.downloadBtn').click(function(){
+	$('.downloadBtns').toggleClass('downloadBtns_active')
+		
+});
+
+$('.downloadBtns').click(function(){
+	$('.downloadBtns').removeClass('downloadBtns_active')
+		
+});
+
+
+$('.hamburger').click(function(){
+	$('.tel_menu').toggleClass('tel_menu_active')
+		
+});
+
+$('.tel_menu').click(function(){
+	$('.tel_menu').removeClass('tel_menu_active')
+		
+});
+
